8dd4bbf66eb6b43bae2a1c7694753d4282c76a19
usermamemichell <barrosmichell23@hotmail.com>
2026-05-08 21:04:42 -0500

    primer ejercicio con python
    empezando a trabajar con python aprendiendo sobre comentarios variables  función print función input  función are y concatenación.

---
+++ proyecto/variables.py
@@ -0,0 +1,35 @@
+#Esto es un comentario de una sola linea 
+"""Esto es un comentario de 
+varias lineas"""
+
+#inicializando variables
+nombre="michell barros moya"
+edad=14
+estado=True
+nota=5.0
+
+#mostrar el contenido de las variables print()
+print(nombre)
+print(edad)
+print(estado)
+print(nota)
+
+#que tipo de datos contiene cada variable.
+print(type(nombre))
+print(type(edad))
+print(type(estado))
+print(type(nota))
+
+#vamos a utilizar la funcion imput para recoger datos por medio del teclado.
+nombre=input("¿como te llamas?")
+edad=input("¿que edad tienes?")
+estado=input("¿en que estado de encuestas?")
+nota=input("¿cual es tu nota?")
+
+#para visualizar que guardamos en las variables anteriores.
+print("hola,", nombre,"un gusto conocerte")
+print("tu edad es:",edad)
+print("tu estado es: ",estado)
+print("tu nota es:",nota)
+
+
